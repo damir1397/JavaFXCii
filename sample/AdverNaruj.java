f6f34e0c627bcaa5abca66cd5c1ffe5962acb289
DAMIR <damir.danzo@gmail.com>
2018-10-13 19:43:37 +0600

    Commit3

--- sample/AdverNaruj.java
+++ sample/AdverNaruj.java
@@ -2,10 +2,11 @@ package sample;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class AdverNaruj {
 
@@ -16,53 +17,52 @@ public class AdverNaruj {
     private URL location;
 
     @FXML
+    private AnchorPane checkBoxD;
+
+    @FXML
     private TextField inputX;
 
     @FXML
     private TextField inputY;
 
     @FXML
+    private CheckBox checkBoxTS;
+
+    @FXML
+    private CheckBox checkBoxTR;
+
+    @FXML
+    private CheckBox checkBoxObsh;
+
+    @FXML
+    private CheckBox checkBoxTC;
+
+    @FXML
+    private CheckBox checkBoxDemog;
+
+    @FXML
+    private CheckBox checkBoxSocial;
+
+    @FXML
+    private CheckBox checkBoxGograf;
+
+    @FXML
+    private CheckBox checkBoxPsyh;
+
+    @FXML
+    private CheckBox checkBoxBaner;
+
+    @FXML
+    private CheckBox checkBoxPlenca;
+
+    @FXML
+    private CheckBox checkBoxPBaner;
+
+    @FXML
+    private CheckBox checkBoxPPlenca;
+
+    @FXML
     private Button buttonZac;
-
-    @FXML
-    void checkBoxBaner(ActionEvent event) {
-
-    }
-
-    @FXML
-    void checkBoxObsh(ActionEvent event) {
-
-    }
-
-    @FXML
-    void checkBoxPBaner(ActionEvent event) {
-
-    }
-
-    @FXML
-    void checkBoxPPlenca(ActionEvent event) {
-
-    }
-
-    @FXML
-    void checkBoxPlenca(ActionEvent event) {
-
-    }
-
-    @FXML
-    void checkBoxTC(ActionEvent event) {
-
-    }
-
-    @FXML
-    void checkBoxTR(ActionEvent event) {
-
-    }
-
-    @FXML
-    void checkBoxTS(ActionEvent event) {
-
-    }
 
     @FXML
     void initialize() {
