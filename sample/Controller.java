91a6660966dd6e8267f6c2a6499da5545b219b88
DAMIR <damir.danzo@gmail.com>
2018-10-15 02:37:06 +0600

    Commit4

--- sample/Controller.java
+++ sample/Controller.java
@@ -1,11 +1,18 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
-public class Controller {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Controller  {
 
     @FXML
     private ResourceBundle resources;
@@ -42,12 +49,13 @@ public class Controller {
 
     @FXML
     void initialize() {
-        adverNaruj.setOnAction(event ->{
-            System.out.println("привет!");
-        });
         adverPress.setOnAction(event ->{
-            System.out.println("привет мир!");
+        System.out.println("привет мир!");
         });
+        adverNaruj.setOnAction(event ->{
+            windomAdd("View/add.fxml");
+            });
+
         adverPrint.setOnAction(event ->{
             System.out.println("привет!");
         });
@@ -69,6 +77,20 @@ public class Controller {
         adverMTS.setOnAction(event ->{
             System.out.println("привет!");
         });
+
+    }
+    public void windomAdd(String windom){
+        FXMLLoader loader=new FXMLLoader();
+        loader.setLocation(getClass().getResource(windom));
+        try {
+            loader.load();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        Parent root=loader.getRoot();
+        Stage stage=new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
 
     }
 }
